d98a3937eb0a193802b2fa04cfc18a3a01aea179
Michael Walker <mwalker39@huskers.unl.edu>
2022-08-23 14:59:46 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Michael Walker
+ * Date: 2022/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Michael Walker\n");
+
+  return 0;
+}
